a2c3227c8ac8d393217a0a5a51c84918d7f4e916
g122622 <51020363+g122622@users.noreply.github.com>
2023-05-20 10:10:43 +0800

    Update ImageViewer.vue

--- src/components/ImageViewer/ImageViewer.vue
+++ src/components/ImageViewer/ImageViewer.vue
@@ -15,20 +15,20 @@
 
 <script setup lang="ts">
 // import { onMounted } from "vue"
-import { ref, onMounted, nextTick, onUnmounted } from "vue"
-import Viewer from 'viewerjs';
-import 'viewerjs/dist/viewer.css'
+// import { ref, onMounted, nextTick, onUnmounted } from "vue"
+// import Viewer from 'viewerjs';
+// import 'viewerjs/dist/viewer.css'
 
-const loading = ref<boolean>(true)
-let viewer: Viewer = null
-interface Props {
+// const loading = ref<boolean>(true)
+// let viewer: Viewer = null
+// interface Props {
     images: Array<{
         src: string
     }>
 }
-const props = defineProps<Props>()
+// const props = defineProps<Props>()
 
-const options = {
+/* const options = {
     inline: false,
     button: true,
     navbar: true,
@@ -58,6 +58,8 @@ onMounted(async () => {
 onUnmounted(async () => {
     viewer.destroy()
 })
+
+*/
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
