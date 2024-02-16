52cb2b770677a54763aab5871aaaa3c866c74c93
g122622 <3300925806@qq.com>
2024-02-16 23:44:49 +0800

    v3.4.1

--- src/test/SQLiteUnit.ts
+++ src/test/SQLiteUnit.ts
@@ -4,31 +4,31 @@
  * Created Date: 2024-01-21 21:54:02
  * Author: Guoyi
  * -----
- * Last Modified: 2024-01-21 22:04:12
+ * Last Modified: 2024-02-16 23:44:31
  * Modified By: Guoyi
  * -----
  * Copyright (c) 2024 Guoyi Inc.
  *
  * ------------------------------------
  */
-import sqlite from "sqlite-electron"
+// import sqlite from "sqlite-electron"
 
-const dbPath = 'E:/暂存/新建文件夹 (45)/test.db'
+// const dbPath = 'E:/暂存/新建文件夹 (45)/test.db'
 
-export default async function testSQLite() {
-    await sqlite.setdbPath(dbPath);
-    await sqlite.executeQuery(`create table meta(
-                    createdDate integer,
-                    modifiedDate integer,
-                    name text,
-                    entryKey text,
-                    versionHash text,
-                    coreVersion text,
-                    comment text
-                )`)
+// export default async function testSQLite() {
+//     await sqlite.setdbPath(dbPath);
+//     await sqlite.executeQuery(`create table meta(
+//                     createdDate integer,
+//                     modifiedDate integer,
+//                     name text,
+//                     entryKey text,
+//                     versionHash text,
+//                     coreVersion text,
+//                     comment text
+//                 )`)
 
-    await sqlite.executeQuery(`create table data(
-                        key text
-                    )`)
-    await sqlite.executeQuery(`insert into data values (?)`, ['gy'])
-}
+//     await sqlite.executeQuery(`create table data(
+//                         key text
+//                     )`)
+//     await sqlite.executeQuery(`insert into data values (?)`, ['gy'])
+// }
